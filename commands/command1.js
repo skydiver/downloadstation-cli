3403d7e7b5c0ee867977897f954f3fcd4c25dc24
Martín M <skydiver@users.noreply.github.com>
2020-04-25 20:47:14 -0300

    project config / added dependencies / cleanup

--- commands/command1.js
+++
@@ -1,5 +0,0 @@
-const print = async (name) => {
-  console.log('Just a console ouput message: Tempor id sit mus sagittis justo senectus torquent augue molestie et congue lobortis nam leo feugiat porta curae fermentum magnis');
-};
-
-module.exports = { print };
