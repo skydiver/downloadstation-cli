6e654effeec9ad1d29315646c070e31edb12315c
Martín M <skydiver@users.noreply.github.com>
2020-04-25 21:26:27 -0300

    store user config

--- commands/config.js
+++ commands/config.js
@@ -1,5 +1,9 @@
 const inquirer = require('inquirer');
 const validator = require('validator');
+const Configstore = require('configstore');
+const keytar = require('keytar');
+
+const packageJson = require('../package.json');
 
 const isUrl = value => {
   const isValid = validator.isURL(value, {
@@ -23,37 +27,31 @@ const isRequired = value => {
 };
 
 const setup = async () => {
-  inquirer
-    .prompt([
-      {
-        type: 'input',
-        name: 'url',
-        message: 'Synology URL with port',
-        validate: isUrl,
-      },
-      {
-        type: 'input',
-        name: 'username',
-        message: 'Your Synology account',
-        validate: isRequired,
-      },
-      {
-        type: 'password',
-        name: 'password',
-        message: 'Your Synology password',
-        validate: isRequired,
-      },
-    ])
-    .then(answers => {
-      // Use user feedback for... whatever!!
-    })
-    .catch(error => {
-      if (error.isTtyError) {
-        // Prompt couldn't be rendered in the current environment
-      } else {
-        // Something else when wrong
-      }
-    });
+  const answers = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'url',
+      message: 'Synology URL with port',
+      validate: isUrl,
+    },
+    {
+      type: 'input',
+      name: 'username',
+      message: 'Your Synology account',
+      validate: isRequired,
+    },
+    {
+      type: 'password',
+      name: 'password',
+      message: 'Your Synology password',
+      validate: isRequired,
+    },
+  ]);
+
+  const config = new Configstore(packageJson.name);
+  config.set('url', answers.url);
+
+  keytar.setPassword('downloadstation-cli', answers.username, answers.password);
 };
 
 module.exports = { setup };
