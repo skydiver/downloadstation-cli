ccbbc01388673c8fa03e813b26558f02a9127bac
Martín M <skydiver@users.noreply.github.com>
2020-04-26 01:27:16 -0300

    display tasks info

--- commands/list.js
+++ commands/list.js
@@ -1,3 +1,6 @@
+const Table = require('cli-table');
+const pretty = require('prettysize');
+
 const ConfigStore = require('../lib/config-store');
 const Synology = require('../lib/synology');
 
@@ -5,7 +8,7 @@ const list = async () => {
   const configStore = new ConfigStore();
   const { url, username, token, password } = await configStore.getAll();
 
-  const synology = new Synology({ url, username, password });
+  const synology = new Synology({ url, username, password, sid: token });
 
   if (!token) {
     const sid = await synology.login();
@@ -21,9 +24,29 @@ const list = async () => {
     configStore.set('token', sid);
   }
 
-  const tasks = await synology.tasks();
+  const tasksResponse = await synology.tasks();
+  const { tasks } = tasksResponse.data;
 
-  console.log(tasks);
+  const ids = tasks.map(task => task.id);
+
+  const tasksInfo = await synology.tasksInfo(ids.join(','));
+  const tasksDetails = tasksInfo.data.tasks;
+
+  const table = new Table({
+    style: { head: ['green'] },
+    head: ['File name', 'File size', 'Downloaded', 'Progress', 'Type'],
+  });
+
+  tasksDetails.forEach(task => {
+    const { status } = tasks.find(t => t.id === task.id);
+    const size = pretty(task.size, { places: 2 });
+    const { size_downloaded } = task.additional.transfer;
+    const downloaded = pretty(size_downloaded, { places: 2 });
+
+    table.push([task.title, size, downloaded, status, task.type]);
+  });
+
+  console.log(table.toString());
 };
 
 module.exports = { list };
